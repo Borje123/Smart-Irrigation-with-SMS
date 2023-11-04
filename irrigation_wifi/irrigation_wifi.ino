de01db7f7723e438431d1440f4570c4a930bef48
Borje123 <93801825+Borje123@users.noreply.github.com>
2023-11-04 08:48:29 +0800

    Add files via upload

---
+++ irrigation_wifi/irrigation_wifi.ino
@@ -0,0 +1,82 @@
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+
+#define FIREBASE_HOST "irrigation-1e918-default-rtdb.firebaseio.com"
+#define FIREBASE_AUTH "aTegM4EqBwC2hrBwIUR0mrrgX7iJfUWbzTqFvEMg"
+
+#define WIFI_SSID "ZTE_2.4G_xs6YrY" //ZTE_2.4G_xs6YrY, PLDTHOMEFIBR272c0
+#define WIFI_PASSWORD "yyinathh"    //yyinathh, PLDTwifi0708)
+
+// WATER SENSOR PIN
+int waterSens = A0;//set water sensor to A0
+
+void setup() {
+  Serial.begin(9600);
+
+ // Connect to WiFi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED) { 
+    Serial.print(".");
+    delay(500);
+
+  }
+
+  Serial.println("\nConnected to Wi-Fi");
+
+  // Initialize Firebase
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+
+    if (Firebase.failed()) {
+    Serial.print("Firebase connection failed: ");
+    Serial.println(Firebase.error());
+  } else {
+    Serial.println("Firebase connected successfully");
+  }
+}
+
+void loop() {
+
+  int sensorValue = analogRead(waterSens);//read the water sensor valu
+  
+  Firebase.setInt("sensor", sensorValue);
+  delay(1000); // Send data every 5 seconds (adjust as needed)
+
+
+  if (sensorValue == 0) {
+
+   
+   Firebase.setString("status", "Low");
+    
+  } 
+  
+  else if (sensorValue > 1 && sensorValue < 300) {
+
+    Firebase.setString("status", "Low");
+    
+    
+  } 
+  
+  else if (sensorValue > 301 && sensorValue < 400) {
+    Firebase.setString("status", "Medium");
+    
+  } 
+  
+  else if (sensorValue > 401){
+    Firebase.setString("status", "High");
+  }
+  else {
+    
+    Serial.println(".");
+  }
+
+  String number = Firebase.getString("num");
+
+  Serial.println(String(number));
+
+   // Send data to Arduino over serial communication
+  Serial.println(sensorValue);
+  delay(1000); // Adjust the delay as needed
+
+
+}
