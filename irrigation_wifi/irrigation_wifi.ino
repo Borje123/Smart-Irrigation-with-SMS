14bfaf55d73d5302a0b0b214ff635220c34ad199
ParmeSean <93801825+DarthCoder-afk@users.noreply.github.com>
2024-04-04 22:30:34 +0800

    Update irrigation_wifi.ino

--- irrigation_wifi/irrigation_wifi.ino
+++ irrigation_wifi/irrigation_wifi.ino
@@ -1,25 +1,24 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
 
-#define FIREBASE_HOST ""
-#define FIREBASE_AUTH ""
+#define FIREBASE_HOST "irrigation-mob-app-default-rtdb.firebaseio.com"
+#define FIREBASE_AUTH "7CxAoV3I0S25iQK2rHJ8nXD1eXUR7FtTF5DMt0n3"
 
-#define WIFI_SSID "" 
-#define WIFI_PASSWORD ""    
+#define WIFI_SSID "ZTE_2.4G_xs6YrY"              //ZTE_2.4G_xs6YrY, PLDTHOMEFIBR272c0, TCL 306, Trojan Horse
+#define WIFI_PASSWORD "yyinathh"  //yyinathh, PLDTwifi0708),kkkkkkkk, admin123
 
 // WATER SENSOR PIN
-int waterSens = A0;//set water sensor to A0
+int waterSens = A0;
 
 void setup() {
   Serial.begin(9600);
 
- // Connect to WiFi.
+  // Connect to WiFi.
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting");
-  while (WiFi.status() != WL_CONNECTED) { 
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(500);
-
   }
 
   Serial.println("\nConnected to Wi-Fi");
@@ -27,7 +26,7 @@ void setup() {
   // Initialize Firebase
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 
-    if (Firebase.failed()) {
+  if (Firebase.failed()) {
     Serial.print("Firebase connection failed: ");
     Serial.println(Firebase.error());
   } else {
@@ -36,47 +35,28 @@ void setup() {
 }
 
 void loop() {
+  int sensorValue = analogRead(waterSens);
 
-  int sensorValue = analogRead(waterSens);//read the water sensor valu
-  
-  Firebase.setInt("sensor", sensorValue);
-  delay(1000); // Send data every 5 seconds (adjust as needed)
+  String recipientData = Firebase.getString("NotifyList");
+  int firstCommaIndex = recipientData.indexOf(',');
+  String sensorData = String(sensorValue) + recipientData.substring(firstCommaIndex);
 
+  Firebase.setString("NotifyList", sensorData);
+
+  Firebase.setInt("SensorValue", sensorValue);
 
   if (sensorValue == 0) {
-
-   
-   Firebase.setString("status", "Low");
-    
-  } 
-  
-  else if (sensorValue > 1 && sensorValue < 300) {
-
-    Firebase.setString("status", "Low");
-    
-    
-  } 
-  
-  else if (sensorValue > 301 && sensorValue < 400) {
-    Firebase.setString("status", "Medium");
-    
-  } 
-  
-  else if (sensorValue > 401){
-    Firebase.setString("status", "High");
-  }
-  else {
-    
+    Firebase.setString("SensorLevel", "Low");
+  } else if (sensorValue > 1 && sensorValue < 389) {
+    Firebase.setString("SensorLevel", "Low");
+  } else if (sensorValue > 390 && sensorValue < 510) {
+    Firebase.setString("SensorLevel", "Medium");
+  } else if (sensorValue > 511) {
+    Firebase.setString("SensorLevel", "High");
+  } else {
     Serial.println(".");
   }
 
-  String number = Firebase.getString("num");
-
-  Serial.println(String(number));
-
-   // Send data to Arduino over serial communication
-  Serial.println(sensorValue);
-  delay(1000); // Adjust the delay as needed
-
-
+  Serial.println(sensorData);
+  delay(1000);  // Adjust the delay as needed
 }
